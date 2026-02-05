210f0bea49418470b1325491ff3e393c0b850e30
at0963 <at0963@srmist.edu.in>
2026-02-05 10:11:44 +0530

    error fixes for streamlit build

--- .streamlit/setup.sh
+++ .streamlit/setup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# Download spaCy model
 python -m spacy download en_core_web_sm
+
+# Download required NLTK data
+python -c "import nltk; nltk.download('punkt'); nltk.download('stopwords'); nltk.download('averaged_perceptron_tagger'); nltk.download('maxent_ne_chunker'); nltk.download('words')"
